0fe612e7477dad42b35d2207ce0acabbb111a260
Alex Aguilar <auza920205@gmail.com>
2022-04-21 16:17:53 -0500

    error and not found handlers added

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import express, { Express } from "express";
 import { userRoutes } from "./app";
 import dotenv from 'dotenv'
+import notFoundHandler from "./middleware/errors/notFoundHandler";
+import errorHandler from "./middleware/errors/errorHandler";
 const app: Express = express();
 const port: number = 8000; // default port to listen
 
+// Loads env files into process.env
 dotenv.config();
 
+// Parses incoming data to JSON
 app.use(express.json());
 
+// User handlers
 app.use('/', userRoutes);
+
+// Undefined routes handler
+app.use(notFoundHandler);
+
+// Error handler
+app.use(errorHandler);
 
 // start the Express server
 app.listen( port, () => {
