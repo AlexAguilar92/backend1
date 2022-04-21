7359030af46331e483d93f4cf5aacd59412828d8
Alex Aguilar <auza920205@gmail.com>
2022-04-21 14:51:54 -0500

    moved

---
+++ src/shared/helpers/implementation/DBConnectionManager.ts
@@ -0,0 +1,28 @@
+import { Connection } from "typeorm";
+import dbConnectionHelper from "./DBConnectionHelper";
+
+export default class DBConnectionManager {
+  connection: Connection;
+
+  async connect(): Promise<Connection> {
+    // tslint:disable-next-line:no-console
+    console.log("DBConnectionManager", "connect");
+    try {
+      if (!this.connection) this.connection = await dbConnectionHelper.connect();
+
+      return this.connection;
+    } catch (error) {
+      // tslint:disable-next-line:no-console
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async disconnect(): Promise<void> {
+    try {
+      await this.connection?.close();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
