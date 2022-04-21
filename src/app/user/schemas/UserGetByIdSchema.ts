96842022d22bbfa9a42ff34acd8346d703e32d27
Alex Aguilar <auza920205@gmail.com>
2022-04-21 17:55:22 -0500

    schema created

---
+++ src/app/user/schemas/UserGetByIdSchema.ts
@@ -0,0 +1,17 @@
+import { ContainerTypes, ValidatedRequestSchema } from "express-joi-validation";
+import Joi from "joi";
+
+interface UserGetByIdSchema extends ValidatedRequestSchema {
+  [ContainerTypes.Params]: {
+    id: string
+  }
+}
+
+const paramsSchema = Joi.object({
+  id: Joi.string().uuid().required()
+});
+
+export {
+  UserGetByIdSchema,
+  paramsSchema
+};
