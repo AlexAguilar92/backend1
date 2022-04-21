d4e46501c422669e573fd907f690a64d8a851ad9
Alex Aguilar <auza920205@gmail.com>
2022-04-21 16:24:04 -0500

    todo created

--- src/repository/entities/Todo.ts
+++ src/repository/entities/Todo.ts
@@ -1,3 +1,17 @@
-export default class Todo {
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import User from "./User";
 
+@Entity({ schema: "public" })
+export default class Todo {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  description: string;
+
+  @Column({ default: true })
+  active: boolean
+
+  @ManyToOne(() => User, (user) => user.todos)
+  user: User;
 }
