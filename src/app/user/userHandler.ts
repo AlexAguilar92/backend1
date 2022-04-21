e1096c3bc8b78484d90abc8c2b248bbcc2c9422a
Alex Aguilar <auza920205@gmail.com>
2022-04-21 14:51:32 -0500

    fixed

--- src/app/user/userHandler.ts
+++ src/app/user/userHandler.ts
@@ -1,26 +1,27 @@
 import express, { Router, Request, Response } from 'express';
 import { UserRepository } from '../../repository';
+import User from '../../repository/entities/User';
 import IUserRepository from '../../repository/user/interface/IUserRepository';
 
 const router: Router = express.Router();
 
 const iUserRepository: IUserRepository = new UserRepository();
 
-router.get('/user/:id', (req: Request, res: Response) => {
+router.get('/user/:id', async (req: Request, res: Response) => {
 
   const { id } = req.params;
 
-  const user = iUserRepository.findById(id as string);
+  const user: User = await iUserRepository.findById(id as string);
 
   res.status(200).send(user);
 
 });
 
-router.get('/user', (req: Request, res: Response) => {
+router.get('/user', async (req: Request, res: Response) => {
 
-  const user = iUserRepository.find();
+  const users: User [] = await iUserRepository.find();
 
-  res.status(200).send(user);
+  res.status(200).send(users);
 
 });
 
