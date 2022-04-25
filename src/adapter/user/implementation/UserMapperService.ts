2309dc711ad6ac6466c9d21dd0f2c6aa647428ef
Alex Aguilar <auza920205@gmail.com>
2022-04-25 17:29:14 -0500

    UserMapper created

---
+++ src/adapter/user/implementation/UserMapperService.ts
@@ -0,0 +1,16 @@
+import IUser from "../../../repository/entities/interface/IUser";
+import MapperService from "../../../shared/utils/mapper/abstraction/MapperService";
+import IUserDTO from "../../DTO/IUserDTO";
+
+export default class UserMapperService extends MapperService<IUser, IUserDTO> {
+  protected map(entity: IUser): IUserDTO {
+    
+    return {
+      id: entity.id,
+      name: entity.name,
+      email: entity.email,
+      status: entity.status
+    }
+  }
+
+}
